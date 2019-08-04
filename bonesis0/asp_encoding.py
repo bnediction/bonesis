c3894b606c480de908b9cb92243caf6016e383a5
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2019-08-04 16:10:43 +0200

    pkn_to_facts: support for unknown signs + conversion to minibn

--- bonesis0/asp_encoding.py
+++ bonesis0/asp_encoding.py
@@ -3,6 +3,8 @@ import clingo as asp
 
 from scipy.special import binom
 
+from functools import reduce
+
 def string_of_facts(facts):
     if not facts:
         return ""
@@ -15,15 +17,19 @@ def print_facts(facts):
 def nb_clauses(d):
     return int(binom(d, d//2))
 
-def pkn_to_facts(pkn, maxclause=None):
+def pkn_to_facts(pkn, maxclause=None, allow_skipping_nodes=False):
     facts = []
-    facts.append(asp.Function("nbnode", [asp.Number(len(pkn.nodes()))]))
-    for n in pkn.nodes():
-        facts.append(asp.Function("node", [asp.String(n)]))
+    if not allow_skipping_nodes:
+        facts.append(asp.Function("nbnode", [asp.Number(len(pkn.nodes()))]))
+        for n in pkn.nodes():
+            facts.append(asp.Function("node", [asp.String(n)]))
+    else:
+        facts.append("nbnode(NB) :- NB = #count{N: node(N)}")
+        for n in pkn.nodes():
+            facts.append("{{{}}}".format(asp.Function("node", [asp.String(n)])))
     for (orig, dest, data) in pkn.edges(data=True):
-        if data["sign"] == "ukn":
-            f = "{{in({},{},(-1;1))}} 1".format(
-                    asp.String(orig), asp.String(dest))
+        if data["sign"] in ["ukn","?"]:
+            f = "in({},{},(-1;1))".format(asp.String(orig), asp.String(dest))
             facts.append(f)
         else:
             s = asp.Number(int(data["sign"]))
@@ -38,4 +44,46 @@ def pkn_to_facts(pkn, maxclause=None):
         facts.append(asp.Function("maxC", [asp.String(n),
             asp.Number(bounded_nb_clauses(i))]))
     return facts
+
+def dnfs_of_facts(fs):
+    bn = {}
+    for d in fs:
+        if d.name == "clause":
+            (i,cid,lit,sign) = d.arguments
+            i = i.string
+            cid = cid.number
+            sign = sign.number
+            lit = lit.string
+            if i not in bn:
+                bn[i] = []
+            if cid > len(bn[i]):
+                bn[i] += [set() for j in range(cid-len(bn[i]))]
+            bn[i][cid-1].add((sign,lit))
+        elif d.name == "constant":
+            (i,v) = d.arguments
+            i = i.string
+            v = v.number
+            bn[i] = v == 1
+    return bn
+
+from colomoto.minibn import BooleanNetwork
+def minibn_of_facts(fs):
+    dnfs = dnfs_of_facts(fs)
+    bn = BooleanNetwork()
+    def make_lit(l):
+        s,v=l
+        v = bn.v(v)
+        if s < 0:
+            v = ~v
+        return v
+    def make_clause(ls):
+        return reduce(lambda l1,l2: l1&l2, map(make_lit, ls))
+    def make_dnf(cs):
+        if isinstance(cs, bool):
+            return cs
+        cs = filter(len, cs)
+        return reduce(lambda c1, c2: c1|c2, map(make_clause, cs))
+    for (node, cs) in sorted(dnfs.items()):
+        bn[node] = make_dnf(cs)
+    return bn
 
