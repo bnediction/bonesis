0aceabf7650893d6e708307b9d61c5c8d1749a33
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2021-02-14 00:52:52 +0100

    fix test

--- tests/test_synthesis.py
+++ tests/test_synthesis.py
@@ -8,8 +8,8 @@ def bodebug(bo):
         fp.write(str(bo.aspmodel))
 
 
-class configurationTest(unittest.TestCase):
-    def test_equal1(self):
+class TestConfiguration(unittest.TestCase):
+    def test_node_equal1(self):
         pkn = bonesis.InfluenceGraph.complete("abc", sign=1, loops=False,
                 allow_skipping_nodes=False)
         data = {
@@ -25,7 +25,7 @@ class configurationTest(unittest.TestCase):
         for _, cfgs in bo.boolean_networks(limit=15, extra="configurations"):
             self.assertEqual(cfgs["x"]["c"], cfgs["y"]["c"])
 
-    def test_equal1(self):
+    def test_node_diff1(self):
         pkn = bonesis.InfluenceGraph.complete("abc", sign=1, loops=False,
                 allow_skipping_nodes=False)
         data = {
