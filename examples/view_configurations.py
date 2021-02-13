188703d1a1ecd603229bd397f7b195e078477f7d
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2021-02-14 00:05:45 +0100

    simple example for extracting configurations

--- examples/view_configurations.py
+++ examples/view_configurations.py
@@ -11,17 +11,7 @@ data = {
 bo = bonesis.BoNesis(pkn, data)
 ~bo.obs("x") >= ~bo.obs("y")
 
-for i, bn in enumerate(bo.boolean_networks(limit=3)):
-    print(f"### {i}")
-    print(bn)
-print("-"*30)
-for i, (bn, cfgs) in enumerate(bo.boolean_networks(limit=3,
-        extra="configurations")):
-    print(f"### {i}")
+for bn, cfgs in bo.boolean_networks(limit=3, extra="configurations"):
+    print("-"*30)
     print(bn, cfgs)
 print("-"*30)
-for i, (bn, cfgs) in enumerate(bo.diverse_boolean_networks(limit=3,
-                        extra="configurations")):
-    print(f"### {i}")
-    print(bn, cfgs)
-
