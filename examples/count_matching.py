5c221211bb0f8b233f05348986e28ffd8349e249
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2021-04-06 23:47:53 +0200

    tests: all_fixpoints in mutant context

--- examples/count_matching.py
+++ examples/count_matching.py
@@ -14,7 +14,21 @@ def all_fixpoints_0(bn):
     return A == [{"a": 0, "b": 0, "c": 0}]
 #count_matching(all_fixpoints_0)
 
+def all_fixpoints_0_mutant(bn):
+    if all_fixpoints_0(bn):
+        bn["a"] = True
+        A = list(bn.attractors())
+        return A == [{"a": 1, "b": 1, "c": 1}]
+    return False
+#count_matching(all_fixpoints_0_mutant)
+
+def all_fixpoints_mutant(bn):
+    bn["a"] = True
+    A = list(bn.attractors())
+    return A == [{"a": 1, "b": 1, "c": 1}]
+count_matching(all_fixpoints_mutant)
+
 def fixpoints_0(bn):
     A = list(bn.attractors())
     return {"a": 0, "b": 0, "c": 0} in A
-count_matching(fixpoints_0)
+#count_matching(fixpoints_0)
--- tests/test_universal.py
+++ tests/test_universal.py
@@ -19,3 +19,13 @@ class TestUniversal(unittest.TestCase):
         self.assertEqual(bo.boolean_networks().count(), 355)
         bo.all_fixpoints({bo.obs("1")})
         self.assertEqual(bo.boolean_networks().count(), 0)
+
+    def test_all_fixpoints_mutant(self):
+        bo = bonesis.BoNesis(self.dom1, self.data1)
+        with bo.mutant({"a": 1}) as m:
+            m.fixed(~m.obs("1"))
+            m.all_fixpoints({m.obs("1")})
+        self.assertEqual(bo.boolean_networks().count(), 2640)
+        bo.fixed(~bo.obs("0"))
+        bo.all_fixpoints({bo.obs("0")})
+        self.assertEqual(bo.boolean_networks().count(), 25)
