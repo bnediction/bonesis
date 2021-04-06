6bc80103fdb960124f5a1c82762af86ebf7048d9
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2021-04-06 23:09:23 +0200

    migrate bn-domain,canonical-bn.asp

--- bonesis/asp_encoding.py
+++ bonesis/asp_encoding.py
@@ -145,9 +145,9 @@ class ASPModel_DNF(object):
         return facts
 
     def encode_domain_InfluenceGraph(self, pkn):
-        self.push_file(aspf("bn-domain.asp"))
+        self.load_template_domain()
         if pkn.canonic:
-            self.push_file(aspf("canonical-bn.asp"))
+            self.load_template_canonic()
         facts = pkn_to_facts(pkn, pkn.maxclause, pkn.allow_skipping_nodes)
         if pkn.exact:
             self.load_template_edge()
@@ -162,6 +162,29 @@ class ASPModel_DNF(object):
                     continue
                 facts.append(clingo.Function("obs", [k, n, s2v(b)]))
         return facts
+
+    @unique_usage
+    def load_template_domain(self):
+        rules = [
+            "{clause(N,1..C,L,S): in(L,N,S), maxC(N,C), node(N), node(L)}",
+            ":- clause(N,_,L,S), clause(N,_,L,-S)",
+            "1 {constant(N,(-1;1)) } 1 :- node(N), not clause(N,_,_,_)",
+            "constant(N) :- constant(N,_)",
+        ]
+        self.push(rules)
+
+    @unique_usage
+    def load_template_canonic(self):
+        rules = [
+            "size(N,C,X) :- X = #count {L,S: clause(N,C,L,S)}; clause(N,C,_,_)",
+            ":- clause(N,C,_,_); not clause(N,C-1,_,_); C > 1",
+            ":- size(N,C1,X1); size(N,C2,X2); X1 < X2; C1 > C2",
+            ":- size(N,C1,X); size(N,C2,X); C1 > C2; mindiff(N,C1,C2,L1) ; mindiff(N,C2,C1,L2) ; L1 < L2",
+            "clausediff(N,C1,C2,L) :- clause(N,C1,L,_);not clause(N,C2,L,_);clause(N,C2,_,_), C1 != C2",
+            "mindiff(N,C1,C2,L) :- clausediff(N,C1,C2,L); L <= L' : clausediff(N,C1,C2,L'), clause(N,C1,L',_), C1!=C2",
+            ":- size(N,C1,X1); size(N,C2,X2); C1 != C2; X1 <= X2; clause(N,C2,L,S) : clause(N,C1,L,S)",
+        ]
+        self.push(rules)
 
     @unique_usage
     def load_template_edge(self):
--- bonesis0/bonesis-asp/bn-domain.asp
+++
@@ -1,4 +0,0 @@
-{clause(N,1..C,L,S): in(L,N,S), maxC(N,C), node(N), node(L)}.
-:- clause(N,_,L,S), clause(N,_,L,-S).
-1 {constant(N,(-1;1)) } 1 :- node(N), not clause(N,_,_,_).
-constant(N) :- constant(N,_).
