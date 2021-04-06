50ebb1d982fd06241b4bdbc922e1b59f5e86285d
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2021-04-06 22:59:52 +0200

    migrate eval.asp

--- bonesis/asp_encoding.py
+++ bonesis/asp_encoding.py
@@ -172,7 +172,16 @@ class ASPModel_DNF(object):
 
     @unique_usage
     def load_template_eval(self):
-        self.push_file(aspf("eval.asp"))
+        rules = [
+            "eval(X,N,C,-1) :- clause(N,C,L,-V), mcfg(X,L,V), not clamped(X,N,_)",
+            "eval(X,N,C,1) :- mcfg(X,L,V): clause(N,C,L,V); clause(N,C,_,_), mcfg(X,_,_), not clamped(X,N,_)",
+            "eval(X,N,1) :- eval(X,N,C,1), clause(N,C,_,_)",
+            "eval(X,N,-1) :- eval(X,N,C,-1): clause(N,C,_,_); clause(N,_,_,_), mcfg(X,_,_)",
+            "eval(X,N,V) :- clamped(X,N,V)",
+            "eval(X,N,V) :- constant(N,V), mcfg(X,_,_), not clamped(X,N,_)",
+            "mcfg(X,N,V) :- ext(X,N,V)",
+        ]
+        self.push(rules)
 
     @unique_usage
     def load_template_cfg(self):
--- bonesis0/bonesis-asp/eval.asp
+++
@@ -1,7 +0,0 @@
-eval(X,N,C,-1) :- clause(N,C,L,-V), mcfg(X,L,V), not clamped(X,N,_).
-eval(X,N,C,1) :- mcfg(X,L,V): clause(N,C,L,V); clause(N,C,_,_), mcfg(X,_,_), not clamped(X,N,_).
-eval(X,N,1) :- eval(X,N,C,1), clause(N,C,_,_).
-eval(X,N,-1) :- eval(X,N,C,-1): clause(N,C,_,_); clause(N,_,_,_), mcfg(X,_,_).
-eval(X,N,V) :- clamped(X,N,V).
-eval(X,N,V) :- constant(N,V), mcfg(X,_,_), not clamped(X,N,_).
-mcfg(X,N,V) :- ext(X,N,V).
