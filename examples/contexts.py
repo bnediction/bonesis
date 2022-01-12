ed33685bf124489f8917610e6a1739a53312b0ae
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2022-01-12 16:24:39 +0100

    minimal example for mutation/action contexts

---
+++ examples/contexts.py
@@ -0,0 +1,18 @@
+import bonesis
+
+dom = bonesis.InfluenceGraph.complete("abc")
+
+bo = bonesis.BoNesis(dom)
+print(bo.reach.iface.stack_manager)
+
+cfg0 = bo.cfg()
+with bo.mutant({"a": 0}) as m:
+    cfg0 >= m.cfg()
+    with bo.action({"b": 1}) as n:
+        cfg1 = n.cfg()
+        cfg0 >= cfg1
+    cfg0 >= m.cfg()
+cfg0 >= m.cfg()
+
+bo.aspmodel.make()
+print(bo.aspmodel)
